9312f43ac8676f3b833602c15714826124d13e29
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-09-29 11:43:47 +0200

    commit exercice node and npm

---
+++ index.js
@@ -0,0 +1,5 @@
+import color from "colors";
+
+console.log(`Je travaille avec Chloé`.rainbow);
+console.log(`Je travaille avec Madeline`.blue);
+console.log(`Je travaille avec Iris`.red);
---
+++ wildersInformation.js
@@ -0,0 +1,7 @@
+const wilderName = "Chloé";
+const school = "Wild Code School";
+
+module.exports = {
+  wilderName: name,
+  school: school,
+};
